0a00d30c21ac0701aa6ee6e95814fb9e65d142da
vlademar <vlademar@mail.ru>
2015-01-12 07:23:21 +0300

    derp

---
+++ TOR/TcpListenerIOState.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace NetGrab.TOR
+{
+    class TcpListenerIOState
+    {
+        public byte[] Buffer { get; private set; }
+        public TcpListener Listener { get; set; }
+        public TcpClient Client { get; set; }
+        public Stream ClientIoStream { get; set; }
+
+        public TcpListenerIOState()
+        {
+            Buffer = new byte[4096];
+        }
+    }
+}
